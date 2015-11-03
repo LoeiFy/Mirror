0af849a0b19bed9951c97ec533349f64951f3738
LoeiFy <loeify@gmail.com>
2015-11-03 17:54:14 +0800

    server

---
+++ gulpfile.js
@@ -0,0 +1,21 @@
+
+var gulp = require('gulp'),
+    connect = require('gulp-connect');
+
+gulp.task('server', function () {
+    return connect.server({
+        port: 2222,
+        livereload: true
+    })
+})
+
+gulp.task('watch', function() {
+    gulp.watch(['*.css', 'index.html'], ['reload'])
+})
+
+gulp.task('reload', function () {
+    return gulp.src(['index.html'])
+        .pipe(connect.reload())
+})
+
+gulp.task('default', ['server', 'watch'])
