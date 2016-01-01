9ebb8346124a11ba31807ac141921f10a9d9728d
LoeiFy <loeify@gmail.com>
2016-01-01 23:45:59 +0800

    markdown

--- assets/mirror.js
+++ assets/mirror.js
@@ -5,18 +5,32 @@ $(function($) {
         issue_single = 'https://api.github.com/repos/LoeiFy/Recordum/issues'+'id',
         issue_comment = 'https://api.github.com/repos/LoeiFy/Recordum/issues'+'id'+'/comments';
 
+    var to_comment = '#new_comment_field';
+
+    /*
     $.ajax({
         type: 'GET',
-        url: 'https://api.github.com/repos/LoeiFy/Recordum/issues/1/comments',
+        url: 'https://api.github.com/repos/LoeiFy/Recordum/issues',
         data: {
-            //filter: 'created',
+            filter: 'created',
             access_token: 'da3fb4003c268a958949'+'6b36b39b5d43a62831b3',
-            //page: 1,
-            //per_page: 2
+            page: 1,
+            per_page: 2
         },
         success: function(data) {
-            console.log(data)
+            console.log(JSON.stringify(data))
         }
     })
+    */
+
+    var html = '<ul>';
+
+    for (var i = 0; i < O.length; i ++) {
+        html += '<li>'+ marked(O[i].body) +'</li>'
+    }
+
+    html += '</ul>';
+
+    $('body').html(html)
 
 })
