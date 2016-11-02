5bc2fd89bb0171832b1e9429d249633bcc4381fb
LoeiFy <loeify@gmail.com>
2016-11-02 15:19:04 +0800

    add

---
+++ src/config.js
@@ -0,0 +1,14 @@
+
+export default {
+
+    user: 'LoeiFy',
+
+    repo: 'Recordum',
+
+    token: 'da3fb4003c268a958949'+'6b36b39b5d43a62831b3',
+
+    per_page: 10,
+
+    about: 'Lorem ipsum dolor sit amet'
+
+}
---
+++ src/index.js
@@ -0,0 +1,10 @@
+
+import axios from 'axios'
+
+import config from './config'
+import api from './api'
+import template from './template'
+
+window.addEventListener('DOMContentLoaded', function() {
+    alert('?')
+})
