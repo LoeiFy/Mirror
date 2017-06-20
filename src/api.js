3a4708aad513ecdc9337289d6e1bf10588150bc5
LoeiFy <LoeiFy@gmail.com>
2017-06-20 22:14:12 +0800

    query

--- src/api.js
+++ src/api.js
@@ -8,3 +8,50 @@ export const ISSUE = (user, repo, id) => `${host}/repos/${user}/${repo}/issues/$
 export const USER = (user) => `${host}/users/${user}`
 
 export const COMMENTS = (user, repo, id) => `${ISSUE(user, repo, id)}/comments`
+
+const api = 'https://api.github.com/graphql'
+
+const schema = {
+  user: `{
+    user(login: "LoeiFy") {
+      name
+      avatarUrl
+      email
+      websiteUrl
+      url
+      bio
+      login
+    }
+  }`,
+  issues: `{
+    repository(owner: "LoeiFy", name: "Recordum") {
+      issues(first: 3, states: OPEN, after: "Y3Vyc29yOnYyOpLOB48Tds4HjxN2", orderBy: {field: CREATED_AT, direction: ASC}) {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          endCursor
+        }
+        edges {
+          node {
+            number
+            title
+            author {
+              avatarUrl
+              login
+              url
+            }
+            updatedAt
+            labels(first: 1) {
+              edges {
+                node {
+                  color
+                  name
+                }
+              }
+            }
+          }
+        }
+      }
+    }
+  }`,
+}
