147b8f1c0cb09baf428fdb3e5a8954168cd0e0fc
loeify <loeify@gmail.com>
2017-07-23 10:09:58 +0800

    creator

--- src/util/index.js
+++ src/util/index.js
@@ -1,3 +1,13 @@
+export function creator(tag, params) {
+  const ele = document.createElement(tag)
+
+  Object.keys(params).forEach((key) => {
+    ele[key] = params[key]
+  })
+
+  return ele
+}
+
 class Query {
   constructor(dom) {
     this.dom = typeof dom === 'string' ?
@@ -14,6 +24,11 @@ class Query {
     return this
   }
 
+  remove(child) {
+    this.dom[0].removeChild(child)
+    return this
+  }
+
   html(html) {
     this.dom.forEach(d => d.innerHTML = html)
     return this
@@ -26,6 +41,13 @@ class Query {
 
   parent() {
     this.dom = [this.dom[0].parentNode]
+    return this
+  }
+
+  append(child) {
+    if (child) {
+      this.dom[0].appendChild(child)
+    }
     return this
   }
 }
