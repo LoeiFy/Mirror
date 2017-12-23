626d018d2011f366a9dccc9eed379fa858fd6ee5
LoeiFy <LoeiFy@gmail.com>
2017-12-23 14:11:11 +0800

    npm

--- test/index.js
+++ test/index.js
@@ -5,6 +5,16 @@ const webpack = require('webpack')
 const WebpackDevServer = require('webpack-dev-server')
 const webpackConfig = require('../webpack.config')
 
+function sleep(t = 0) {
+  return new Promise(resolve => {
+    setTimeout(resolve, t)
+  })
+}
+
+function getPostLength() {
+  return document.querySelectorAll('.post').length
+}
+
 const compiler = webpack(webpackConfig)
 const devServerOptions = Object.assign({}, webpackConfig.devServer)
 const server = new WebpackDevServer(compiler, devServerOptions)
@@ -13,6 +23,23 @@ server.listen(1234, '127.0.0.1', () => {
   console.log('Server on http://127.0.0.1:1234')
 })
 
-// const browser = await puppeteer.launch({ headless: false })
-// const page = await browser.newPage()
-// await page.goto('http://127.0.0.1:1234')
+describe('acyort', async () => {
+  it('run', async function () {
+    this.timeout(20000)
+
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto('http://127.0.0.1:1234')
+    const watch = page.mainFrame().waitForFunction(`document.documentElement.classList.contains('loading')===false`)
+    await watch
+    await sleep(1000)
+
+    const postLength = await page.evaluate(getPostLength)
+    assert(postLength === 5)
+
+    // const post = await page.$('.post')
+    // await post.click()
+    await browser.close()
+    server.close()
+  })
+})
